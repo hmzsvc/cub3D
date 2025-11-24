937818d1556ed9842d02f61076dc2b5edf9ba1ba
Hamza Sivaci <hasivaci@k2m07s01.42kocaeli.com.tr>
2025-11-24 15:54:18 +0300

    add vision fpv triangle

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hasivaci <hasivaci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 15:45:28 by hasivaci          #+#    #+#             */
-/*   Updated: 2025/11/20 18:36:14 by hasivaci         ###   ########.fr       */
+/*   Updated: 2025/11/24 15:32:30 by hasivaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,7 @@ char **get_map(void)
 
 void init_game(t_game *game)
 {
+    init_player(&game->player);
     game->mlx = mlx_init();
     game->map = get_map();
     game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Game");
@@ -84,6 +85,32 @@ void init_game(t_game *game)
     game->data = mlx_get_data_addr(game->img, &game->bbp, &game->size_line, &game->endian);
     mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
 }
+bool touch (float px, float py, t_game *game)
+{
+    int x = px / BLOCK;
+    int y = py / BLOCK;
+    
+    if (game->map[y][x] == '1')
+        return(true);
+    return(false);
+}
+
+void draw_line(t_player *player, t_game *game, float start_x, int i)
+{
+    float cos_angle = cos(start_x);
+    float sin_angle = sin(start_x);
+    float ray_x = player->x;
+    float ray_y = player->y;
+    
+    while (!touch(ray_x, ray_y, game))
+    {
+        put_pixel(ray_x, ray_y,0xFF0000, game);
+        ray_x += cos_angle;
+        ray_y += sin_angle;
+    }
+    
+}
+
 
 int draw_loop(t_game *game)
 {
@@ -92,6 +119,19 @@ int draw_loop(t_game *game)
     clear_image(game);
     draw_square(player->x, player->y, 15, 0x00FF00, game);
     draw_map(game);
+
+    float fraction = PI / 3 / WIDTH;
+    float start_x = player->angle - PI / 6;
+    int i = 0;
+    while (i < WIDTH)
+    {
+        draw_line(player, game, start_x, i);
+        start_x += fraction;
+        i++;
+    }
+    
+   
+    
     mlx_put_image_to_window(game->mlx, game->win, game->img, 0,0);
     return(0);
 }
--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: hasivaci <hasivaci@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 15:38:26 by hasivaci          #+#    #+#             */
-/*   Updated: 2025/11/20 17:22:15 by hasivaci         ###   ########.fr       */
+/*   Updated: 2025/11/24 15:23:31 by hasivaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 void init_player(t_player *player)
 {
     player->x = WIDTH / 2;
+    // player->x = 200;
     player->y = HEIGHT / 2;
-
+    // player->y = 200;
+    player->angle = PI / 2;
+    
     player->key_up = false;
     player->key_down = false;
     player->key_right = false;
     player->key_left = false;
+    
+    player->left_rotate = false;
+    player->right_rotate = false;
 }
 
 int key_press(int keycode, t_player *player)
@@ -33,6 +39,11 @@ int key_press(int keycode, t_player *player)
         player->key_left = true;
     if (keycode == D)
         player->key_right = true;
+    
+    if (keycode == LEFT)
+        player->left_rotate = true;
+    if (keycode == RIGHT)
+        player->right_rotate = true;
     return(0);
 }
 int key_release(int keycode, t_player *player)
@@ -45,12 +56,28 @@ int key_release(int keycode, t_player *player)
         player->key_left = false;
     if (keycode == D)
         player->key_right = false;
+    if (keycode == LEFT)
+        player->left_rotate = false;
+    if (keycode == RIGHT)
+        player->right_rotate = false;
     return(0);
 }
 
 void move_player(t_player *player)
 {
     int speed = 5;
+    float angle_speed = 0.1;
+    float cos_angle = cos(player->angle);
+    float sin_angle = sin(player->angle);
+
+    if (player->left_rotate)
+        player->angle -= angle_speed;
+    if (player->right_rotate)
+        player->angle += angle_speed;
+    if (player->angle > 2 * PI)
+        player->angle = 0;
+    if (player->angle < 0)
+        player->angle = 2 * PI;
 
     if (player->key_up)
         player->y -= speed;
@@ -60,4 +87,25 @@ void move_player(t_player *player)
         player->x -= speed;
     if (player->key_right)
         player->x += speed;
+
+    // if (player->key_up)
+    // {
+    //     player->x += cos_angle * speed;
+    //     player->y += sin_angle * speed;
+    // }
+    // if (player->key_down)
+    // {
+    //     player->x -= cos_angle * speed;
+    //     player->y -= sin_angle * speed;
+    // }
+    // if (player->key_left)
+    // {
+    //     player->x += cos_angle * speed;
+    //     player->y -= sin_angle * speed;
+    // }
+    // if (player->key_right)
+    // {
+    //     player->x -= cos_angle * speed;
+    //     player->y += sin_angle * speed;
+    // }
 }
