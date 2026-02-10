56024f5c7279ec15a9aa497cb526a318e377090f
hozturk777 <huseyinro03@gmail.com>
2026-02-11 01:44:43 +0300

    Added unkown character check

--- srcs/map_utils_dir/map_control.c
+++ srcs/map_utils_dir/map_control.c
@@ -6,7 +6,7 @@
 /*   By: hsyn <hsyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 16:09:51 by hsyn              #+#    #+#             */
-/*   Updated: 2026/02/10 22:49:26 by hsyn             ###   ########.fr       */
+/*   Updated: 2026/02/11 00:50:27 by hsyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,22 @@
 // Map'in sol sütun kısmı için x = 0 y++ kontrol edilecek
 // Map'in en sağ sütun kısmı için x max value y++ kontrol edilecek
 // map'in alt kısmı için y max value x++ kontrol edilecek
+static void	unkown_character_check(char c)
+{
+	int	map_component;
+	int	player_component;
+	int	whitespace_component;
+	
+	map_component = (c != '1' && c != '0');
+	player_component = (c != 'S' && c != 'N' && c != 'E' && c != 'W');
+	whitespace_component = (c != ' ' && c != '\t');
+	if (map_component && player_component && whitespace_component)
+		printf("Unkown Character Error!\n");
+	
+	//if (c != '1' && c != '0' && c != 'S' && c != 'N' && c != 'E' && c != 'W' && c != ' ')
+	//	printf("Unkown Character Error!\n");
+}
+
 void	wall_control_util(int x, int y)
 {
 	int		is_wall;
@@ -41,6 +57,7 @@ void	wall_control()
 		while (game->map[y][x])
 		{
 			wall_control_util(x, y);
+			unkown_character_check(game->map[y][x]);
 			x++;
 		}
 		game->map_width = x;
@@ -48,4 +65,5 @@ void	wall_control()
 	}
 }
 
-// Map'in ortasında da boşluk olabilir ve çevresi wall olmak zorunda ayrı kontrol lazım
+// Map'in ortasında da boşluk olabilir ve çevresi wall olmak zorunda ayrı kontrol lazım ('0' çevresinde boşluk karakteri varsa hata dönülebilir kısa yol)
+// Map'de istenilmeyen karakter kontorlü burada yapılabilir bir flag olacak (is_wall) gibi olması gerek karakter harici karakter gelirse '1' olacak ve hata dönecek
