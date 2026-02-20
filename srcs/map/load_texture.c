a3bc7334cf0ed4f9ae9570ac3ce7a61fdfc618f0
hozturk777 <huseyinro03@gmail.com>
2026-02-20 04:13:29 +0300

    Tex path check added

--- srcs/map/load_texture.c
+++ srcs/map/load_texture.c
@@ -6,7 +6,7 @@
 /*   By: hsyn <hsyn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 15:23:24 by hsyn              #+#    #+#             */
-/*   Updated: 2026/02/20 00:19:42 by hsyn             ###   ########.fr       */
+/*   Updated: 2026/02/20 04:12:05 by hsyn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,33 @@ static int	load_tex(t_texture	*tex, char *path)
 	
 	tex->img = mlx_xpm_file_to_image(game->mlx, path, &tex->width, &tex->height);
 	if (!tex->img)
+	{
+		printf("Texture path not found\n");
+		clear_garbage();
 		return (0);
+	}
 	tex->addr = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len, &tex->endian);
 	return (1);
+}
+
+#include <fcntl.h>
+#include <unistd.h>
+
+
+static void	text_path_check(char *path)
+{
+	int	fd;
+	t_game	*game;
+
+	game = global_game();
+	fd = open(path, O_RDONLY);
+    if (fd < 0)
+	{
+		//clear_garbage();
+		close_game(game);
+        exit (1);
+	}
+    close(fd);
 }
 
 void	load_all_tex()
@@ -34,24 +58,26 @@ void	load_all_tex()
 	t_game	*game;
 	
 	game = global_game();
-	game->n_path = whitespaces_term(game->n_path); // NULL CHECK EKLENECEK
-	game->s_path = whitespaces_term(game->s_path); // NULL CHECK EKLENECEK
-	game->e_path = whitespaces_term(game->e_path); // NULL CHECK EKLENECEK
-	game->w_path = whitespaces_term(game->w_path); // NULL CHECK EKLENECEK
-	if (!game->n_path || !game->s_path || !game->e_path || !game->w_path)
-	{
-		printf("Texture path not found\n");
-		clear_garbage();
-		exit(1);
-	}
+	
+	//game->n_path = whitespaces_term(game->n_path); // NULL CHECK EKLENECEK
+	//game->s_path = whitespaces_term(game->s_path); // NULL CHECK EKLENECEK
+	//game->e_path = whitespaces_term(game->e_path); // NULL CHECK EKLENECEK
+	//game->w_path = whitespaces_term(game->w_path); // NULL CHECK EKLENECEK
+	//if (!game->n_path || !game->s_path || !game->e_path || !game->w_path)
+	//	exit(1);
+	text_path_check(game->n_path); // BURADA HATA VE LEAK VAR
+	text_path_check(game->s_path);
+	text_path_check(game->e_path);
+	text_path_check(game->w_path);
+
 	if (!load_tex(&game->n_tex, game->n_path))
-		exit (1);
+		exit(1);
 	if (!load_tex(&game->s_tex, game->s_path))
-		exit (1);
+		exit(1);
 	if (!load_tex(&game->e_tex, game->e_path))
-		exit (1);
+		exit(1);
 	if (!load_tex(&game->w_tex, game->w_path))
-		exit (1);
+		exit(1);
 }
 
 int	get_tex_pixel(t_texture *tex, int x, int y)
